510961d65c4603677162b1a094dcd430e739c936
Alin Grigorescu <alin.grig87@gmail.com>
2022-01-14 20:25:29 +0200

    [Return a JSON array of Product objects]

--- src/main/java/com/grig/demo/DemoApplication.java
+++ src/main/java/com/grig/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.grig.demo;
 
+import com.grig.demo.product.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +18,13 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Product> hello() {
+		return List.of(
+				new Product(1L, "iphone X", 999.99, "Iphone X",
+						"https://shop.flip.ro/serve-device-photos?n=apple_iphone-x_silver_sell.jpg&s=1000&t=device-images"),
+				new Product(2L, "iphone XS", 1099.99, "Iphone XS",
+						"https://shop.flip.ro/serve-device-photos?n=apple_iphone-xs_space-grey_sell.jpg&s=1000&t=device-images")
+		);
 	}
 
 }
